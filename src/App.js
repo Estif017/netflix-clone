3700d7f1a5bef509a038006267325979196337e8
Estif017 <estifoteklearegay@gmail.com>
2020-11-05 15:19:56 -0800

    added styled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './components/pages/home';
 
 function App() {
   return (
     <div>
-     Netflix
+     <Home/>
     </div>
   );
 }
---
+++ src/components/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Jumbotron from '../jumbotron/index'
+
+export default function Home() {
+    return (
+        <Jumbotron.Container>
+            <Jumbotron.Title>Jumbotron</Jumbotron.Title>
+            <Jumbotron.SubTitle>Hello Jumbotron</Jumbotron.SubTitle>
+        </Jumbotron.Container>
+    )
+}
