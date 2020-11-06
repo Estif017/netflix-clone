66637d4667e3154647cb3949b056bf02fc8c67fb
Estif017 <estifoteklearegay@gmail.com>
2020-11-06 13:45:12 -0800

    created footer and an accordian for footer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import AccordionContainer from '../containers/Accordion'
+import FooterContainer from '../containers/Footer'
 import JumbotronContainer from '../containers/Jumbotron'
 
 export default function Home() {
     return (
-        <JumbotronContainer/>
+        <>
+            <JumbotronContainer/>
+            <FooterContainer/>
+            <AccordionContainer/>
+        </>
     )
 }
